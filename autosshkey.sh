b93aaad285e912919608fef32fe6cf5ad7ebf610
JB1366 <jb8745@proton.me>
2026-09-08 18:48:58 -0500

    AutoSSHKey

--- autosshkey.sh
+++ autosshkey.sh
@@ -285,6 +285,7 @@ node_auth() {
 	if [ ! -s "$SSH_KEY" ]; then echo -e "\n$YL[!] Main Router SSH Key not found.$NC"; pause; return; fi
     echo -e "\n$GR[✓] Main Router SSH Key found at: $WH$SSH_KEY$NC\n"
     if [ -z "$SSH_NODES" ] || [ "$SSH_NODES" = " " ]; then node_choice="2"; EMPTY_NODES="1"
+    elif [ "$RETRY" = "1" ]; then node_choice="2"
     else
         echo -e "$BL Node Source Selection: \n"
         echo -e "  $N1 Use saved node(s) from CONFIG"
@@ -398,7 +399,7 @@ node_auth() {
                 return ;;
             *)
                 printf "\n$BL[i] Retrying authentication...$NC"; sleep 5
-                echo -e ""; node_auth; return ;;
+                echo -e ""; RETRY="1"; node_auth; return ;;
         esac
     fi
 }
